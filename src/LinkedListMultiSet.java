f43e8d6c14610253e70f0814945e056c71540a40
Alisa Dubina <adubina24@stu.aberdeenhall.com>
2025-09-22 16:12:52 -0400

    Rewrite LinkedListMultiSet class in Java.

--- src/LinkedListMultiSet.java
+++ src/LinkedListMultiSet.java
@@ -25,23 +25,51 @@ public class LinkedListMultiSet extends MultiSet {
     }
 
     public void remove(int item) {
-
-    }
+        Node cur = front;
+        Node prev = null;
+        while (cur != null){
+            if (cur.item == item) {
+                size -= 1;
+                if (prev != null){
+                    prev.next = cur.next;
+                }
+                else {
+                    // removing the first node
+                    front = cur.next;
+                } // if
+                return;  // stop after removing the first occurrence
+            }
+            prev = cur;
+            cur = cur.next;
+        } // while
+    } // remove
 
     public boolean contains(int item) {
+        Node cur = front;
+        while (cur != null){
+            if (cur.item == item)
+                return true;
+            cur = cur.next;
+        } // while
         return false;
-    }
+    } // contains
 
     public boolean isEmpty() {
-        return false;
-    }
-
+        return this.front == null;
+    } // isEmpty
 
     public int count(int item) {
-        return -1;
-    }
+        int num_seen = 0;
+        Node cur = this.front;
+        while (cur != null){
+            if (cur.item == item)
+                num_seen++;
+            cur = cur.next;
+        } // while
+        return num_seen;
+    } // count
 
     public int size() {
-        return -1;
-    }
-}
+        return this.size;
+    } // size
+} // LinkedListMultiSet
